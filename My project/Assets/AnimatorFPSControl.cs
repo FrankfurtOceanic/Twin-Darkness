c5df056210d650fdf0c871659b5d7b768d8912d1
Joey Marten <joey.marten@mail.mcgill.ca>
2023-08-24 20:21:51 -0400

    Added frame limiter script

---
+++ My project/Assets/AnimatorFPSControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFPSControl : MonoBehaviour
+{
+    [SerializeField] private  float fps = 24;
+    private Animator animator;
+    private bool playing;
+    private float timeSinceLastFrame = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        Debug.Log(animator.name);
+        animator.StopPlayback();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.StopPlayback();
+        timeSinceLastFrame += Time.deltaTime;
+        if (timeSinceLastFrame > 1 / fps)
+        {
+            animator.enabled = true;
+            animator.Update(timeSinceLastFrame);
+            animator.enabled = false;
+            
+            timeSinceLastFrame = 0;
+        }
+
+
+    }
+}
